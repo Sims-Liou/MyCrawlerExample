7610a60639f92002432f2977a2e7354954f08372
Sims-Liou <gip24000@gmail.com>
2016-12-10 10:31:50 +0800

    Test commit and push

--- src/crawler/example/AppleRealtime.java
+++ src/crawler/example/AppleRealtime.java
@@ -21,7 +21,7 @@ import com.google.common.collect.ImmutableSortedMap;
  * @author Abola Lee
  *
  */
-//test commit and push
+//Test commit and push
 public class AppleRealtime {
 	
 	static SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHH:mm");
